183156ac0986dd8e0e0a3dcb5ad0931ea61eb69c
Hirotaka <yokokawa.491992@gmail.com>
2018-11-17 22:49:43 +0900

    hukusixyuu-warikann 綺麗になった! intは()でくくらなくて大丈夫｡

--- hukusixyuu-warikan.py
+++ hukusixyuu-warikan.py
@@ -1,5 +1,5 @@
-amount = (int(input("合計金額を教えてね(円):")))
-customer = (int(input("人数を教えてね(人):")))
+amount = int(input("合計金額を教えてね(円):"))
+customer = int(input("人数を教えてね(人):"))
 
 print(f"一人あたり:{amount // customer}円"),print(f"端数:{amount % customer}円")
 
